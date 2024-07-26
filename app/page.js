ce28a63344e987a66b504340cdb7cdd86a266a66
Abijeeth Vasra <abijeeth.vasra@bounteous.com>
2024-07-26 15:20:55 +0530

    key error fix

--- app/page.js
+++ app/page.js
@@ -19,14 +19,14 @@ export default function Home() {
           <h2 className="text-4xl font-bold mb-4">Welcome to Our Blog</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
       </main>
-      <div className="flex justify-end px-4">
+      <div className="flex justify-end px-4 py-4">
         <input type="text" className="px-4 py-2 border border-gray-300 rounded-md" placeholder="Search..." />
         <button className="px-4 py-2 bg-blue-500 text-white rounded-md ml-4">Search</button>
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-5">
         {posts.map((post) => (
           <Link href={"/post/"+ post._id}>
-            <div className="border border-gray-200 p-4">
+            <div key={post._id} className="border border-gray-400 p-4">
               <img className="w-full h-48 object-cover mb-4" src={post.image} alt="Post Image"/>
               <h2 className="text-xl font-semibold mb-2">{post.title}</h2>
               <p className="text-gray-600">{post.short_description}</p>
